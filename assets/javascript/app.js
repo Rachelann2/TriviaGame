8e92434adaeba61e22f94345f18c585259f0d262
Rachels Mac <rasilverthorn@ucdavis.edu>
2019-10-19 14:49:44 -0700

    final review

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,102 @@
+var quizText = document.getElementById("quiz")
 
-var guesses = ["click"];
-var answerwrong = "X";
-// var answerright = "O";
-// var quizText = document.getElementById("quiz")
+var Startbtn = $("#Startbutton")
+var count = 30;
+var submitbtn = $("#Submit")
+var question1 = document.getElementsByClassName("question1")
 
-// document.onkeyup = function (event) {
+var answerColor = $("#D1, #B2, #C3, #D4, #B5, #A6")
+var submitAns = $("#Submit")
+var clickedanswer1 = $("span.answers1")
+var clickedanswer2 = $("span.answers2")
+var clickedanswer3 = $("span.answers3")
+var clickedanswer4 = $("span.answers4")
+var clickedanswer5 = $("span.answers5")
+var clickedanswer6 = $("span.answers6")
 
-//     var userGuess = event.key;
+var answer1 = $("#A1")
+var gameHasStarted = false;
 
-//     guesses.push(userGuess);
+
+clickedanswer1.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
+
+clickedanswer2.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
+
+clickedanswer3.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
+
+clickedanswer4.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
+
+clickedanswer5.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
+
+clickedanswer6.one("click", "button.answers", function (event) {
+
+    if ((gameHasStarted)) {
+        $(event.target).css("background-color", "yellow");
+    }
+
+});
 
 
-//     if ((UserGuess === "click")) {
-//         answerright++;
+submitAns.on("click", function () {
+    $(answerColor).css("background-color", "red");
+    console.log("hi")
 
-//     }
+});
 
-//     else {
-//         answerwrong--;
-//     }
 
-//     quizText.textContent = "";
+Startbtn.one("click", function () {
+    gameHasStarted = true;
 
-$("button").on("click", function () {
-    var count = 30;
+
     var interval = setInterval(function () {
         document.getElementById('count').innerHTML = "Time Remaining: " + count;
         count--;
-        if (count === 0) {
+
+        if (count <= 0) {
             clearInterval(interval)
             document.getElementById('count').innerHTML = ("Times up!");
+            quizText = $(answerColor).css("background-color", "blue");
         }
+
         else {
-
         }
-    }, 1000);
-    console.log(interval)
 
+    }, 1000);
 
 
 });
+
+
+
 
 
